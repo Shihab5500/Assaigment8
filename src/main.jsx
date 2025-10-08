2c2a55c5e88a22bab15e92f3776973d96c7a958d
Shihab5500 <Shihabahmed6501@gmail.com>
2025-10-08 15:12:59 +0600

    some delete

--- src/main.jsx
+++ src/main.jsx
@@ -1,47 +1,3 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import './index.css'
-// import App from './App.jsx'
-
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// )
-
-
-
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import { RouterProvider } from "react-router-dom";
-// import router from "./router/router";
-// import "./index.css";
-
-// ReactDOM.createRoot(document.getElementById("root")).render(
-//   <React.StrictMode>
-//     <RouterProvider router={router} />
-//   </React.StrictMode>
-// );
-
-
-
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import { RouterProvider } from "react-router-dom";
-// import router from "./router/router";
-// import { InstallProvider } from "./context/InstallContext";
-// import "./index.css";
-
-// ReactDOM.createRoot(document.getElementById("root")).render(
-//   <React.StrictMode>
-//     <InstallProvider>
-//       <RouterProvider
-//         router={router}
-//         future={{ v7_startTransition: true }}  // v7 future flag (optional warning fix)
-//       />
-//     </InstallProvider>
-//   </React.StrictMode>
-// );
 
 
 import React from "react";
@@ -58,3 +14,4 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     </InstallProvider>
   </React.StrictMode>
 );
+
